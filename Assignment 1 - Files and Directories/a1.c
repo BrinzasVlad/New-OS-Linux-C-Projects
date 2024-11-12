40e4409e1fd87f587ca92f2448cdc4b41afa07b2
Vlad Brinzas <vlad.brinzas@gmail.com>
2024-11-12 15:51:07 +0200

    Added stub solution and Docker support
    Currently we can build the Docker image with
    `docker build --tag os-assig-1 --file a1_dockerfile .`
    and run it with
    `docker run os-assig-1`
    and it runs successfully. (All tests fail, since nothing is
    currently implemented, of course.)

---
+++ Assignment 1 - Files and Directories/a1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    // If no implemented option is found, display an error message
+    printf("ERROR\n");
+    printf("invalid option");
+    
+    return 0;
+}
