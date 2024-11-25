bff5327633a8b18fe12d280a5e568c46b4e5e28a
Vlad Brinzas <vlad.brinzas@gmail.com>
2024-11-25 22:14:04 +0200

    Implemented 'Same-Process Thread Synchronization' in Assignment 2
    Also added generic dummy threads for all other processes that must create
    threads, so that one only needs to implement the specific special threads
    in each process now.

--- Assignment 2 - Threads and Processes/a2.c
+++ Assignment 2 - Threads and Processes/a2.c
@@ -1,6 +1,23 @@
 #include <unistd.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include "a2_helper.h"
+
+struct os_assig_2_thread_data {
+    int process_id;
+    int thread_id;
+};
+
+void* generic_thread(void* thread_info_as_void) {
+    struct os_assig_2_thread_data data = *(struct os_assig_2_thread_data*)thread_info_as_void;
+    
+    info(BEGIN, data.process_id, data.thread_id);
+    
+    info(END, data.process_id, data.thread_id);
+    
+    return NULL;
+}
 
 void process_8() {
     info(BEGIN, 8, 0);
@@ -8,8 +25,92 @@ void process_8() {
     info(END, 8, 0);
 }
 
+struct process_7_synchronization {
+    pthread_mutex_t guard;
+    pthread_cond_t thread_5_started_cond;
+    bool thread_5_started;
+    pthread_cond_t thread_2_exited_cond;
+    bool thread_2_exited;
+};
+
+void* process_7_thread_2(void* process_7_sync_as_void) {
+    struct process_7_synchronization *process_7_sync = (struct process_7_synchronization*)process_7_sync_as_void;
+    
+    // Wait for T7.5 to start
+    pthread_mutex_lock(&process_7_sync->guard);
+        while (!process_7_sync->thread_5_started) {
+            pthread_cond_wait(&process_7_sync->thread_5_started_cond, &process_7_sync->guard);
+        }
+    pthread_mutex_unlock(&process_7_sync->guard);
+    
+    info(BEGIN, 7, 2);
+    
+    // Terminate and notify T7.5
+    pthread_mutex_lock(&process_7_sync->guard);
+        info(END, 7, 2);
+        process_7_sync->thread_2_exited = true;
+        pthread_cond_signal(&process_7_sync->thread_2_exited_cond);
+    pthread_mutex_unlock(&process_7_sync->guard);
+    
+    return NULL;
+}
+
+void* process_7_thread_5(void* process_7_sync_as_void) {
+    struct process_7_synchronization* process_7_sync = (struct process_7_synchronization*)process_7_sync_as_void;
+    
+    // Start and notify T7.2
+    pthread_mutex_lock(&process_7_sync->guard);
+        info(BEGIN, 7, 5);
+        process_7_sync->thread_5_started = true;
+        pthread_cond_signal(&process_7_sync->thread_5_started_cond);
+    pthread_mutex_unlock(&process_7_sync->guard);
+    
+    // Wait for T7.2 to end
+    pthread_mutex_lock(&process_7_sync->guard);
+        while (!process_7_sync->thread_2_exited) {
+            pthread_cond_wait(&process_7_sync->thread_2_exited_cond, &process_7_sync->guard);
+        }
+    pthread_mutex_unlock(&process_7_sync->guard);
+    
+    info(END, 7, 5);
+    
+    return NULL;
+}
+
 void process_7() {
     info(BEGIN, 7, 0);
+    
+    struct process_7_synchronization process_7_sync;
+    pthread_mutex_init(&process_7_sync.guard, NULL);
+    pthread_cond_init(&process_7_sync.thread_5_started_cond, NULL);
+    process_7_sync.thread_5_started = false;
+    pthread_cond_init(&process_7_sync.thread_2_exited_cond, NULL);
+    process_7_sync.thread_2_exited = false;
+    
+    pthread_t thread_ids[5];
+    struct os_assig_2_thread_data thread_arguments[5];
+    // The above is a bit inefficient, since T7.2 and T7.5 don't use theirs
+    // But the space waste is minor enough to ignore (and might be optimized out anyway)
+    for (int thread_number = 1; thread_number <= 5; ++thread_number) {
+        int thread_index = thread_number - 1;
+        switch (thread_number) {
+            case 2:
+                pthread_create(&thread_ids[thread_index], NULL, process_7_thread_2, &process_7_sync);
+                break;
+            case 5:
+                pthread_create(&thread_ids[thread_index], NULL, process_7_thread_5, &process_7_sync);
+                break;
+            default:
+                thread_arguments[thread_index].process_id = 7;
+                thread_arguments[thread_index].thread_id = thread_number;
+                pthread_create(&thread_ids[thread_index], NULL, generic_thread, &thread_arguments[thread_index]);
+        }
+        
+    }
+    
+    for (int i = 0; i < 5; ++i) {
+        pthread_join(thread_ids[i], NULL);
+    }
     
     info(END, 7, 0);
 }
@@ -21,6 +122,18 @@ void process_6() {
     if (0 == process_8_pid) {
         process_8();
         return;
+    }
+    
+    pthread_t thread_ids[6];
+    struct os_assig_2_thread_data thread_arguments[6];
+    for (int i = 0; i < 6; ++i) {
+        thread_arguments[i].process_id = 6;
+        thread_arguments[i].thread_id = i + 1;
+        pthread_create(&thread_ids[i], NULL, generic_thread, &thread_arguments[i]);
+    }
+    
+    for (int i = 0; i < 6; ++i) {
+        pthread_join(thread_ids[i], NULL);
     }
     
     waitpid(process_8_pid, NULL, 0);
@@ -40,6 +153,18 @@ void process_4() {
     if (0 == process_7_pid) {
         process_7();
         return;
+    }
+    
+    pthread_t thread_ids[35];
+    struct os_assig_2_thread_data thread_arguments[35];
+    for (int i = 0; i < 35; ++i) {
+        thread_arguments[i].process_id = 4;
+        thread_arguments[i].thread_id = i + 1;
+        pthread_create(&thread_ids[i], NULL, generic_thread, &thread_arguments[i]);
+    }
+    
+    for (int i = 0; i < 35; ++i) {
+        pthread_join(thread_ids[i], NULL);
     }
     
     waitpid(process_7_pid, NULL, 0);
@@ -104,13 +229,7 @@ void process_1() {
 }
 
 int main() {
-    // Note: normally we should try to handle errors encountered
-    // when trying to start new processes or threads. However, for
-    // this assignment in particular there isn't much we can do
-    // if a such operation fails, save maybe for retrying, because
-    // the assignment explicitly requires an exact process hierarchy.
-    
-    init(); // Call the tester and announce we are starting
+    init();
 
     process_1();
     
